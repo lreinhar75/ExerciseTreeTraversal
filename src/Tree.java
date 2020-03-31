c4c2713642d87e0c05d8baf2a768fab855d71e83
lreinhar75 <liner@ruc.dk>
2020-03-31 15:46:32 +0200

    Solution

--- src/Tree.java
+++ src/Tree.java
@@ -32,31 +32,62 @@ public class Tree {
     }
 
 
-
+//Finish the next functions
     public void BFT() {
+        Queue<Node> nodeQueue = new LinkedList<Node>();
+        Node c;
+        if (this.root != null)
+            nodeQueue.add(this.root);
+        while (!nodeQueue.isEmpty())
+        {
+            c = nodeQueue.remove();
+            System.out.print(c.data +", ");
+            if (c.leftChild != null)
+                nodeQueue.add(c.leftChild);
+            if (c.rightChild !=null)
+                nodeQueue.add(c.rightChild);
+        }
+
+
     }
-
-
     public void DFTPreOrder() {
         System.out.print("\n Preorder DFT: ");
         DFTPreOrder(this.root);
     }
-    public void DFTPreOrder(Node n)
-    {
+    public void DFTPreOrder(Node n) {
+        if (n==null)
+            return;
+        System.out.print(n.data+ ", ");
+        DFTPreOrder(n.leftChild);
+        DFTPreOrder(n.rightChild);
     }
     public void DFTInOrder() {
         System.out.print("\n Inorder DFT: ");
         DFTInOrder(this.root);
     }
     public void DFTInOrder(Node n){
+        if (n==null)
+            return;
+        DFTInOrder(n.leftChild);
+        System.out.print(n.data+ ", ");
+        DFTInOrder(n.rightChild);
+
     }
     public void DFTPostOrder() {
         System.out.print("\n Postorder DFT: ");
         DFTPostOrder(this.root);
     }
     public void DFTPostOrder(Node n){
+        if (n==null)
+            return;
+        DFTPostOrder(n.leftChild);
+        DFTPostOrder(n.rightChild);
+        System.out.print(n.data+ ", ");
     }
 }
+//The methods for tree traversal are in the tree Class
+
+//This is the node class do not need to do anything here but you may make a getData method
 
 class Node <T extends Comparable<T>> {
     public T data;
